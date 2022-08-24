4de60a5be0e5f04e34d85685f31216375081e2ee
yoonwongoo <yunyun26@naver.com>
2022-08-24 15:16:24 +0900

    환경설정세팅완료

---
+++ src/main/java/com/study/junitStudy/JunitStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.junitStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/junitStudy/JunitStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.junitStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
